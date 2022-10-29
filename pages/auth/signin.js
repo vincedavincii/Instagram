e065bea4d65615b39397a351998784e8f1403670
vincedavincii <99784095+vincedavincii@users.noreply.github.com>
2022-10-29 05:13:53 +0300

    created the Signin page with goole authentication

---
+++ pages/auth/signin.js
@@ -0,0 +1,51 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import {getProviders, signIn, SignOut} from "next-auth/react";
+import Header from "../../components/Header";
+
+const signin = ({providers}) => {
+	return (
+		<>
+			<Header />
+			<div className="flex justify-center space-x-7 mt-20">
+				<img
+					src="https://like4like.com/img/hero-x750.png"
+					alt="instagram photo"
+					className="h-80 hidden md:inline-flex object-cover rotate-6 md:w-48"
+				/>
+				<div>
+					{Object.values(providers).map((provider) => (
+						<div
+							key={provider.name}
+							className="flex flex-col items-center"
+						>
+							<img
+								src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmGBzIpMRYQjbxkVg0FB8cwhTjIvSfWpjPNQ&usqp=CAU"
+								alt="instagram logo"
+								className="w-20 object-cover text-center"
+							/>
+							<p className="text-sm italic my-10">
+								This Webpage is created for learning purposes
+							</p>
+							<button
+								onClick={() => signIn(provider.id, {callbackurl: "/"})}
+								className="bg-red-400 rounded-md p-3 text-white hover:bg-red-500"
+							>
+								Sign in with {provider.name}
+							</button>
+						</div>
+					))}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export const getServerSideProps = async (context) => {
+	const providers = await getProviders();
+	return {
+		props: {providers},
+	};
+};
+
+export default signin;
